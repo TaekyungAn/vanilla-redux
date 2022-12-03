e6eca4e7b91349ab7802ddc5134a008f88654011
atk0426@naver.com <taekyung@DESKTOP-HRK1UQR.localdomain>
2022-12-03 22:08:58 +0900

    Subscriptions

--- src/index.js
+++ src/index.js
@@ -1,24 +1,37 @@
-import { createStore } from 'redux';
+import { createStore } from "redux";
 // createSotre에 계속 취소선 나와서 검색해 보니, reduxtoolkit사용 권장을 위해서라고 한다.
 // 따라서 나는 버전을 낮췄지만, 그러지 않을 경우는 다음과 같이 사용할 수 있다.
 // import { legacy_createStore as createStore } from 'redux';
 // 출처: https://okky.kr/article/1209744
-
 const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-const countModifier = (state = 0, action) => {
+number.innerText = 0;
+
+const countModifier = (count = 0, action) => {
   console.log(count, action);
-  if (action.type === "ADD"){
-    return count +1;
-  }else if(action.type === "MINUS"){
-    return count -1;
-  }else{
+  if (action.type === "ADD") {
+    return count + 1;
+  } else if (action.type === "MINUS") {
+    return count - 1;
+  } else {
     return count;
   }
 };
 
 const countStore = createStore(countModifier);
 
-add.addEventListener("click", () => )
+const onChange = () => {
+  number.innerText = countStore.getState();
+};
+countStore.subscribe(onChange);
+
+const handleAdd = () => {
+  countStore.dispatch({ type: "ADD" });
+};
+const handleMinus = () => {
+  countStore.dispatch({ type: "MINUS" });
+};
+add.addEventListener("click", handleAdd);
+minus.addEventListener("click", handleMinus);

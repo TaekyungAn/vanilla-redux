f460256a35b2d8e40533b69f239250deae272852
atk0426@naver.com <taekyung@DESKTOP-HRK1UQR.localdomain>
2022-12-03 23:16:06 +0900

    2.1 State Mutation

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ const reducer = (state = [], action) => {
   console.log(action);
   switch (action.type) {
     case ADD_TODO:
-      return [];
+      return [...state, { text: action.text, id: Date.now() }];
     case DELETE_TODO:
       return [];
     default:
@@ -23,6 +23,8 @@ const reducer = (state = [], action) => {
 };
 
 const store = createStore(reducer);
+
+store.subscribe(() => console.log(store.getState()));
 
 const onSubmit = (e) => {
   e.preventDefault();

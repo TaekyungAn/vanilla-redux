747d60096cd9d1cdd426f9a77b257626d2275ec6
atk0426@naver.com <taekyung@DESKTOP-HRK1UQR.localdomain>
2022-12-03 23:09:30 +0900

    2.0 Vanilla ToDo

--- src/index.js
+++ src/index.js
@@ -3,38 +3,32 @@ import { createStore } from "redux";
 // 따라서 나는 버전을 낮췄지만, 그러지 않을 경우는 다음과 같이 사용할 수 있다.
 // import { legacy_createStore as createStore } from 'redux';
 // 출처: https://okky.kr/article/1209744
-const add = document.getElementById("add");
-const minus = document.getElementById("minus");
-const number = document.querySelector("span");
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+const ul = document.querySelector("ul");
 
-number.innerText = 0;
+const ADD_TODO = "ADD_TODO";
+const DELETE_TODO = "DELETE_TODO";
 
-const ADD = "ADD";
-const MINUS = "MINUS";
-
-const countModifier = (count = 0, action) => {
+const reducer = (state = [], action) => {
+  console.log(action);
   switch (action.type) {
-    case ADD:
-      return count + 1;
-    case MINUS:
-      return count - 1;
+    case ADD_TODO:
+      return [];
+    case DELETE_TODO:
+      return [];
     default:
-      return count;
+      return state;
   }
 };
 
-const countStore = createStore(countModifier);
+const store = createStore(reducer);
 
-const onChange = () => {
-  number.innerText = countStore.getState();
+const onSubmit = (e) => {
+  e.preventDefault();
+  const toDo = input.value;
+  input.value = "";
+  store.dispatch({ type: ADD_TODO, text: toDo });
 };
-countStore.subscribe(onChange);
 
-const handleAdd = () => {
-  countStore.dispatch({ type: ADD });
-};
-const handleMinus = () => {
-  countStore.dispatch({ type: MINUS });
-};
-add.addEventListener("click", handleAdd);
-minus.addEventListener("click", handleMinus);
+form.addEventListener("submit", onSubmit);
